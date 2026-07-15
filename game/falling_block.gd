223101b6e3af57dca49ddf161b531376f8fbc6ee
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-07-15 13:42:47 -0500

    fix falling block terminal velocity

--- game/falling_block.gd
+++ game/falling_block.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
 @export var gravity := 800.0
-@export var terminal_velocity := 300.0
+@export var terminal_velocity := 700.0
 
 
 func _physics_process(delta: float) -> void:
-	velocity.y += clampf(gravity * delta, -terminal_velocity, terminal_velocity)
+	velocity.y = clampf(
+		velocity.y + gravity * delta,
+		-terminal_velocity,
+		terminal_velocity,
+	)
 	move_and_slide()
