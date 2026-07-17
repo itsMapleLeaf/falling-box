1ecdf09df37146745b2d458a7551d3069187e634
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-07-17 12:07:18 -0500

    falling block: use quad mesh

--- game/falling_block.gd
+++ game/falling_block.gd
@@ -7,24 +7,24 @@ const MAX_LIFETIME = 15.0
 
 var lifetime: float
 
-@onready var sprite: ColorRect = %ColorRect
+@onready var sprite: MeshInstance2D = %Sprite
 @onready var collision_shape: CollisionShape2D = %CollisionShape
 
 
 func _ready() -> void:
 	lifetime = MAX_LIFETIME
-	sprite.color.a = 0
+	sprite.modulate.a = 0
 
 
 func _process(delta: float) -> void:
 	lifetime -= delta
 	if lifetime > 0:
-		sprite.color.a = minf(sprite.color.a + delta, 1)
+		sprite.modulate.a = minf(sprite.modulate.a + delta, 1)
 	if lifetime < 0:
-		sprite.color.a = maxf(sprite.color.a - delta, 0)
+		sprite.modulate.a = maxf(sprite.modulate.a - delta, 0)
 		collision_layer = 0
 		collision_mask = 0
-		if sprite.color.a == 0:
+		if sprite.modulate.a == 0:
 			queue_free()
 
 
