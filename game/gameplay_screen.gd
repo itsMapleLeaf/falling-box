13fa418e660413c2aabd3d479ebb8f1f7367c9cd
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-07-17 12:08:46 -0500

    add falling blocks group
    so they get layered behind the player (and the held block / grab dot)

--- game/gameplay_screen.gd
+++ game/gameplay_screen.gd
@@ -11,6 +11,7 @@ var level_bounds := Rect2i(0, 0, 0, 0)
 
 @onready var debug_menu: GameplayDebugMenu = %DebugMenu
 @onready var player: CharacterBody2D = %Player
+@onready var falling_blocks: Node2D = %FallingBlocks
 
 
 func _ready() -> void:
@@ -29,8 +30,10 @@ func _ready() -> void:
 
 func _respawn_player() -> void:
 	player.global_position = (
-			Vector2(level_bounds.get_center()) * Constants.LEVEL_CELL_SIZE
-			- Vector2(0, player_spawn_height)
+		Vector2(level_bounds.get_center()) * Constants.LEVEL_CELL_SIZE - Vector2(
+				0,
+				player_spawn_height,
+		)
 	)
 	player.velocity = Vector2.ZERO
 
@@ -45,11 +48,10 @@ func _create_level_block() -> LevelBlock:
 func _on_block_spawn_timer_timeout() -> void:
 	var block: Node2D = FALLING_BLOCK.instantiate()
 	block.global_position = Vector2(
-		randi_range(level_bounds.position.x, level_bounds.end.x - 1)
-		* Constants.LEVEL_CELL_SIZE,
-		-falling_block_spawn_height,
+			randi_range(level_bounds.position.x, level_bounds.end.x - 1) * Constants.LEVEL_CELL_SIZE,
+			-falling_block_spawn_height,
 	)
-	add_child(block)
+	falling_blocks.add_child(block)
 
 
 func _debug_spawn_block_above_player():
