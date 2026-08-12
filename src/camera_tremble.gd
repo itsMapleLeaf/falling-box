1f34342ee7050585bae35d00d4baa755c7df0a6d
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-08-11 23:24:12 -0500

    lint/error fix

--- src/camera_tremble.gd
+++ src/camera_tremble.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
-@onready var debug_label: Label = %DebugLabel
 
-
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	position = Vector2.from_angle(randf() * TAU) * Tremble.current_intensity
 	reset_physics_interpolation()
