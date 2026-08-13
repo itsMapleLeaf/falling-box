72c5daf45bcb034aa30d1ed94b062e9a4ba418e6
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-08-12 19:04:24 -0500

    add exit_screen callback
    also move the _set_screen function to the top i guess because whatever

--- src/main.gd
+++ src/main.gd
@@ -6,6 +6,16 @@ const NETWORK_MENU = preload("uid://i4jvedvwx3en")
 var screen: Screen
 
 
+func _set_screen(new_screen: Screen) -> void:
+	if screen:
+		screen.exit_screen()
+		screen.queue_free()
+
+	screen = new_screen
+	screen.screen_changed.connect(_set_screen)
+	add_child(screen)
+
+
 func _ready() -> void:
 	var args := OS.get_cmdline_user_args()
 	match Array(args):
@@ -25,12 +35,3 @@ func _ready() -> void:
 		_:
 			printerr("unknown arguments:", args)
 			_set_screen(NETWORK_MENU.instantiate())
-
-
-func _set_screen(new_screen: Node) -> void:
-	if screen:
-		screen.queue_free()
-
-	screen = new_screen
-	screen.screen_changed.connect(_set_screen)
-	add_child(screen)
--- src/screen.gd
+++ src/screen.gd
@@ -6,3 +6,7 @@ signal screen_changed(new_screen: Screen)
 
 func _change_screen(new_screen: Screen) -> void:
 	screen_changed.emit(new_screen)
+
+
+func exit_screen() -> void:
+	pass
