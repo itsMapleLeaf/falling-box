da087b1f8781ee73af9ca5dfae016005480529fe
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-08-12 19:39:10 -0500

    debug log ui

--- src/debug_ui.gd
+++ src/debug_ui.gd
@@ -1,15 +1,35 @@
 extends CanvasLayer
 
-@onready var label: Label = $Label
+const MAX_LOG_COUNT := 100
 
+var logs: Array[String]
 var debug_values: Dictionary[String, Variant] = { }
 
+@onready var log_message_list: VBoxContainer = %LogMessageList
+@onready var log_message_template: Label = %LogMessageTemplate
+@onready var log_scroll_container: ScrollContainer = %LogScrollContainer
+@onready var debug_value_label: Label = %DebugValues
 
-func set_debug_value(name: String, value: Variant) -> void:
-	debug_values[name] = value
+
+func _ready() -> void:
+	log_message_template.hide()
 
 
-func _process(delta: float) -> void:
-	label.text = ""
+func set_debug_value(value_name: String, value: Variant) -> void:
+	debug_values[value_name] = value
+
+	debug_value_label.text = ""
 	for key in debug_values:
-		label.text += "%s: %s\n" % [key, str(debug_values[key])]
+		debug_value_label.text += "%s: %s\n" % [key, str(debug_values[key])]
+
+
+func log(msg: Variant) -> void:
+	var message_node := log_message_template.duplicate()
+	message_node.text = str(msg)
+	message_node.show()
+
+	log_message_list.add_child(message_node)
+	log_scroll_container.get_v_scroll_bar().value = 1
+
+	if log_message_list.get_child_count() > MAX_LOG_COUNT:
+		log_message_list.get_child(0).queue_free()
