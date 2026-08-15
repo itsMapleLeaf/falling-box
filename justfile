0423a7368f41bc63d72ee7308ac46d90648e9d90
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-08-15 03:00:08 -0500

    simple play test and justfile just because

---
+++ justfile
@@ -0,0 +1,8 @@
+play:
+    godot
+
+debug:
+    godot_console --debug
+
+test:
+    godot_console --headless --path . --script addons/gut/gut_cmdln.gd
---
+++ src/main_menu_test.gd
@@ -0,0 +1,19 @@
+extends GutTest
+
+
+func test_play_starts_local_game():
+	var menu := Screens.main_menu()
+	add_child_autofree(menu)
+
+	var play_button: Button = menu.get_node("%PlayButton")
+	play_button.emit_signal("pressed")
+
+	assert_not_null(
+		ScreenManager.current_screen,
+		"The current screen should be set after pressing Play",
+	)
+	assert_eq(
+		ScreenManager.current_screen.get_class(),
+		Screens.local_game().get_class(),
+		"Pressing Play should open the local game screen",
+	)
