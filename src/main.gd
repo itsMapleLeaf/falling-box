1b295ae96e3114145a79b21b823c43cdecf25b41
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-08-17 14:45:41 -0500

    fix more type warnings

--- src/main.gd
+++ src/main.gd
@@ -7,12 +7,12 @@ func _ready() -> void:
 		['host', var port]:
 			var game: ServerGame = Screens.server_game()
 			ScreenManager.set_screen(game)
-			game.host_server(int(port))
+			game.host_server(str(port).to_int())
 
 		['join', var host, var port]:
 			var game: ServerGame = Screens.server_game()
 			ScreenManager.set_screen(game)
-			game.join_server(host, int(port))
+			game.join_server(str(host), str(port).to_int())
 
 		['play']:
 			ScreenManager.set_screen(Screens.local_game())
