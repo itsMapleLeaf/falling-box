191c8b39c318a3cd6ec7758268d9f2b16b52f6e6
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-08-18 12:04:39 -0500

    remove unneeded random data structures

--- src/online/player_state.gd
+++
@@ -1,7 +0,0 @@
-class_name PlayerState
-extends Node
-
-@export var player_id := randi()
-@export var peer_id: int
-@export var alias := ""
-@export var is_ready := false
--- src/online/room_state.gd
+++
@@ -1,4 +0,0 @@
-class_name RoomState
-extends Node
-
-@export var players: Array[PlayerState] = []
