f23bebe19ab6b6a7130fd7a9f1ebdf34c82f7bad
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2026-08-18 12:05:20 -0500

    remove ready label for now

--- src/online/lobby/player_status.gd
+++ src/online/lobby/player_status.gd
@@ -5,8 +5,6 @@ extends HBoxContainer
 
 @onready var name_label: Label = %Name
 @onready var self_label: Label = %SelfLabel
-@onready var ready_label: Label = %Ready
-@onready var not_ready_label: Label = %NotReady
 
 
 func _ready() -> void:
@@ -19,8 +17,3 @@ func set_player_name(player_name: String) -> void:
 
 func set_player_is_self(is_self: bool) -> void:
 	self_label.visible = is_self
-
-
-func set_player_ready(is_player_ready: bool) -> void:
-	ready_label.visible = is_player_ready
-	not_ready_label.visible = not is_player_ready
